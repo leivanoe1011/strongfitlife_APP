523a90047116591dd95949d216773c196e0eb9d8
8connor <connorh16@gmail.com>
2021-02-04 12:11:19 -0500

    Adding some style changes for the header on the personal training component.

--- client/src/Components/PersonalTraining/Header.js
+++ client/src/Components/PersonalTraining/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Col from "react-bootstrap/Col";
+// import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
@@ -7,27 +7,11 @@ function Header() {
     return (
         <div className="parallax2">
             <Container>
-                <Row>
-                    <Col
-                        sm={12}
-                        md={12}
-                        lg={6}
-
-                        className=""
-                    >
-                        
-                    </Col>
-                    <Col
-                        sm={12}
-                        md={12}
-                        lg={6}
-
-                        className=""
-                    >
-
-                    </Col>
+                <Row className="text-center">
+                    <h1 className="headerTitle"> 1:1 PERSONAL TRAINING</h1>
                 </Row>
             </Container>
+
         </div>
     )
 }
