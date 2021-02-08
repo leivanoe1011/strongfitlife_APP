fcde5c0b80478eb8364c8eff61d8d791718c8156
8connor <connorh16@gmail.com>
2021-02-08 11:46:00 -0500

    Working on the contact form. I think it would be best to set up email js.

---
+++ client/src/Components/Contact/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
+function Header() {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Header;
