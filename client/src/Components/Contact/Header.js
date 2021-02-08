3732770d8681a60d4832c7f9763c4e299514cded
8connor <connorh16@gmail.com>
2021-02-08 12:42:54 -0500

    Contact form is finished for the large sized screens.

--- client/src/Components/Contact/Header.js
+++ client/src/Components/Contact/Header.js
@@ -4,7 +4,9 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import nowPic from "../../images/nowPic.jpg";
 import Image from "react-bootstrap/Image";
-import "./styles.css"
+import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import "./styles.css";
 
 function Header() {
     return (
@@ -36,8 +38,32 @@ function Header() {
 
                 </Col>
             </Row>
-            <Row>
-
+            <Row className="justify-content-center">
+                <Form>
+                    <Form.Row>
+                        <Col>
+                            <Form.Control size="lg" type="text" placeholder="first name" />
+                        </Col>
+                        <Col>
+                            <Form.Control size="lg" type="text" placeholder="last name" />
+                        </Col>
+                    </Form.Row>
+                    <Form.Row className="mt-3">
+                        <Col>
+                            <Form.Control size="lg" type="email" placeholder="email" />
+                        </Col>
+                    </Form.Row>
+                    <Form.Row className="mt-3 mb-5">
+                        <Col>
+                            <Form.Control as="textarea" size="lg" type="email" placeholder="email" />
+                        </Col>
+                    </Form.Row>
+                    <Form.Row className="mt-3 mb-5">
+                        <Button type="submit">
+                            Submit
+                        </Button>
+                    </Form.Row>
+                </Form>
             </Row>
         </Container>
     )
